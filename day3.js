243c74b908812c6e9986d1ebbb73ef2b4282ef00
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2022-12-03 12:13:36 +0530

    Day3 part2 completed

--- day3.js
+++ day3.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 
 const input = fs.readFileSync("./data/day3.txt", "utf-8");
 
-const findCommonCharacter = (firstValue, secondValue) => {
-  return firstValue
+const findCommonCharacter = (values) => {
+  return values[0]
     .split("")
-    .filter((character) => secondValue.includes(character));
+    .filter((character) => values[1].includes(character) && values[2].includes(character));
 };
 
 const divideValues = (currentValue) => {
@@ -14,18 +14,26 @@ const divideValues = (currentValue) => {
 };
 
 const main = (data) => {
-  return data.split("\n").reduce((totalPriority, currentValue) => {
-    const [firstHalf, secondHalf] = divideValues(currentValue);
-    const commonValue = [
-      ...new Set(findCommonCharacter(firstHalf, secondHalf)),
-    ];
+  const values = data.split("\n");
+  const input = [];
+  let currentIndex = 0;
+  for (let index = 0; index < values.length; index++) {
+    if (index % 3 === 0) {
+      currentIndex++;
+      input[currentIndex] = [];
+    }
+    input[currentIndex].push(values[index]);
+  }
+  input.shift();
+  return input.reduce((totalPriority, currentValue) => {
+    const commonValue = [... new Set(findCommonCharacter(currentValue))]
     const value = commonValue.reduce((totalvalue, cur) => {
-      const commonValue1 = cur.charCodeAt(0);
-      return commonValue1 < 91
-        ? totalvalue + commonValue1 - 38
-        : totalvalue + commonValue1 - 96;
-    }, 0);
-    return totalPriority + value;
+        const commonValue1 = cur.charCodeAt(0);
+        return commonValue1 < 91
+          ? totalvalue + commonValue1 - 38
+          : totalvalue + commonValue1 - 96;
+      }, 0);
+      return totalPriority + value;
   }, 0);
 };
 
