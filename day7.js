8ba32c8797de46fd9281de7d946480434a2179b6
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2022-12-08 08:23:00 +0530

    Day 7 completed

--- day7.js
+++ day7.js
@@ -1,66 +1,68 @@
-const { dir } = require("console");
 const fs = require("fs");
 
-// part 1 1307902
-// part 2 7068748
+const temp1 = fs.readFileSync('./data/day7.txt', "utf-8").split("\n")
 
-const input = fs.readFileSync("./data/day7.txt", "utf-8").trimEnd();
-
-const getNodes = (instructions) => {
-  let currentId = 0;
-  const root = [
-    { name: "/", type: "dir", size: 0, parent: null, id: currentId },
-  ];
-  const directoryIdMap = { "/": 0 };
-  for (
-    let instructionIndex = 1;
-    instructionIndex < instructions.length;
-    instructionIndex++
-  ) {
-    const [command, instruction] = instructions[instructionIndex].split(" ");
-    if (command.startsWith("ls")) continue;
-    if (command.startsWith("cd")) {
-      currentId =
-        instruction === ".."
-          ? directoryIdMap[root[currentId].parent]
-          : directoryIdMap[instruction];
-      continue;
+let sum=0; 
+let directory= {'/':{size:0}}; 
+let cwd=['/']; 
+temp1.forEach( x=> {
+    x=x.split(' '); 
+    if(x[0]==='$') {        
+        if(x[1]==='cd') {
+            if(x[2]==='..') {
+                cwd.pop();
+            }
+            else {
+                const dirname=x[2];
+                cwd.push(dirname); 
+                const fullpath=cwd.join('/');
+                directory[fullpath]={};
+                directory[fullpath].size=0;
+            }
+        }
     }
-    if (command === "dir") {
-      root.push({
-        name: instruction,
-        type: "dir",
-        size: 0,
-        parent: root[currentId].name,
-        id: root.length,
-      });
-      directoryIdMap[instruction] = root.length - 1;
-    } else {
-      root.push({
-        name: instruction,
-        type: "file",
-        size: +command,
-        parent: root[currentId].name,
-        id: root.length,
-      });
+    else if(x[0]==='dir') return;
+    else {
+        const fileSize=parseInt(x[0]);
+        const temp=[];        
+        cwd.forEach(d=> {
+            temp.push(d);            
+            const temp2=temp.join('/');
+            directory[temp2].size+=fileSize;    
+        });
     }
-  }
-  return [root, directoryIdMap];
-};
+});
+for(dir in directory) {
+    if(directory[dir].size<=100000)sum+=directory[dir].size; 
+}
+console.log(sum);
 
+const sizeToFree=directory['/'].size-70000000+30000000
+let closestSize=70000000;
+for(dir in directory) {
+    if(directory[dir].size>sizeToFree && directory[dir].size<closestSize)closestSize=directory[dir].size;
+}
+console.log(directory)
+console.log('Answer 1: ', sum);
+console.log('Answer 2: ', closestSize);
 
+// const updateParentValue = (valueToAdd, parent, idMap, nodes) => {
+//     if(parent != null) {
+//         nodes[idMap[parent]].size += valueToAdd;
+//         updateParentValue(valueToAdd, parent.parent, idMap, nodes)
+//     }
+// }
 
-const main = (data) => {
-  const instructions = data
-    .split("\n")
-    .map((instruction) => instruction.replace("$ ", ""));
-  const [nodes, directoryIdMap] = getNodes(instructions);
-  console.log(nodes, directoryIdMap)
-  return nodes
-    .filter((node) => node.size < 100000 && node.type === "dir")
-    .reduce((total, node) => {
-      return (total += node.size);
-    }, 0);
-};
+// const calculateSize = (nodes, directoryIdMap) => {
+//   nodes
+//     .filter((node) => node.type === "file")
+//     .forEach((node) => {
+//       updateParentValue(node.size, node.parent, directoryIdMap, nodes);
+//     });
+//   nodes
+//     .filter((node) => node.type === "file")
+//     .forEach((node) => {
+//       updateParentValue(node.size, node.parent, directoryIdMap, nodes);
+//     });
+// };
 
-console.log(main(input));
